6ffc743e46be7720cff01a0dd4e353122abdb8cf
Bruno Ochotorena <hackerhilt@gmail.com>
2023-12-19 02:41:43 -0300

    Add player functionality and update table points

--- src/game.rs
+++ src/game.rs
@@ -10,13 +10,13 @@ pub struct Game {
     players: Vec<Player>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Manager {
-    games: Vec<Game>,
-    tables: Vec<Table>,
+#[derive(Debug, Serialize)]
+pub struct Manager<'a> {
+    pub games: Vec<Game>,
+    tables: Vec<Table<'a>>,
 }
 
-impl Manager {
+impl Manager<'_> {
     pub fn new_with_games(number: usize) -> Self {
         let tables = Vec::with_capacity(number);
         let games = (0..number).map(|_| Game::default()).collect();
@@ -26,19 +26,32 @@ impl Manager {
 }
 
 impl Game {
-    pub fn run(self) -> Table {
+    pub fn run(&self) -> Table {
         println!("Game is running");
         Table::new(self)
     }
+
+    pub fn add_player(&mut self, player: &str) {
+        let player = Player {
+            name: player.to_string(),
+        };
+
+        self.players.push(player);
+    }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Table {
-    game: Game,
+#[derive(Debug, Serialize)]
+pub struct Table<'a> {
+    game: &'a Game,
+    points: u8,
 }
 
-impl Table {
-    pub fn new(game: Game) -> Self {
-        Table { game }
+impl<'a> Table<'a> {
+    pub fn new(game: &'a Game) -> Self {
+        Table { game, points: 0 }
+    }
+
+    pub fn add_one_point(&mut self) {
+        self.points += 1;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,16 @@ use crate::game::Manager;
 
 #[tokio::main]
 async fn main() {
-    let manager = Manager::new_with_games(10);
+    let mut manager = Manager::new_with_games(10);
+    let game = manager.games.first_mut().unwrap();
+
+    game.add_player("John");
+    game.add_player("Jane");
+
+    let mut table = game.run();
+    table.add_one_point();
+
+    game.add_player("Jack");
 
 
     println!("Hello, world!");
